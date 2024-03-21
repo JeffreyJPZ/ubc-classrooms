72237a326fddf595cf6cb90d1caa5083cf60fbc4
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-03-20 18:06:02 -0700

    docs: added comments and stub for compute_timeslots

--- scripts/compute_timeslots.py
+++ scripts/compute_timeslots.py
@@ -1,17 +1,25 @@
 """
 Uses the raw booking data to calculate available timeslots for classrooms throughout the academic year
 """
-
+import json
 import pandas as pd
 from enum import Enum
 from datetime import datetime, timedelta
 from pathlib import Path
 
 from models import TimetableSettings
+from models import BuildingCode
 
 
 
 TARGET_DIR = '/timeslot_data' # Directory to write files to
+
+
+
+def compute_timeslots(building_code : BuildingCode) -> None:
+    # Reads booking data from file for a building, gets the available timeslots for the entire academic year, and writes the timeslots to file
+
+    return 0 # stub
 
 
 
@@ -26,9 +34,24 @@ def main() -> None:
     #       - Each timeslot spans the largest continuous interval possible between the end of one booking and the start of another booking
     #       - No two timeslots overlap
     # Writes the timeslot data to file
-    
-    return 0 # stub
 
+    # TODO: replace with compute_timeslots_config for prod
+    configPath = 'config/compute_timeslots_config_test.json'
+
+    with open(Path(__file__).parent / configPath, encoding='utf8') as f:
+        building_code_data = json.load(f)
+
+        # Validate building codes
+        for building_code in building_code_data['buildingCodes']:
+            try:
+                assert building_code == BuildingCode[building_code].name
+            except AssertionError:
+                print("An invalid building code was entered\n")
+                return
+
+        for building_code in building_code_data['buildingCodes']:
+            compute_timeslots(BuildingCode[building_code])
+        
 
 
 main()
