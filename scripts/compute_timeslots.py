ade4d104324b9f24337a44688483e276ed6f9e59
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-03-20 19:41:00 -0700

    feat: added write to file functionality for compute_timeslots

--- scripts/compute_timeslots.py
+++ scripts/compute_timeslots.py
@@ -14,11 +14,69 @@ from models import BuildingCode
 
 
 
+def get_table_headers() -> dict[str, int]:
+    # Return a mapping of attributes for an available timeslot within an academic year to their column index in the timeslot table:
+    # Campus:                   UBCV
+    # Year:                     Academic year in format YYYY-YYYY
+    # Building:                 3-5 letter code representing a building
+    # Room:                     3-4 digit classroom number
+    # RoomType:                 String representing whether the room the is a general teaching space or a restricted space
+    # Start:                    Datetime in ISO-8601 format representing the beginning of the timeslot
+    # End:                      Datetime in ISO-8601 format representing the end of the timeslot
+    return {
+        "Campus": 0, 
+        "Year": 1,
+        "Building": 2, 
+        "Room": 3, 
+        "RoomType": 4,
+        "Start": 5,
+        "End": 6
+    }
+
+
+
+def compute_timeslots_by_rooms(dataframe, building_code : BuildingCode) -> list[list[str]]:
+    return []
+
+
+
+def write_to_file(data : list[str[str]], building_code : BuildingCode) -> None:
+    # Writes the timeslot data to file
+
+    # Ensure that scraped table columns are in the correct order
+    table_headers = get_table_headers()
+    # Initialize number of columns
+    columns = [None] * len(table_headers)
+    for header in table_headers:
+        # Inserts the header at the correct index
+        columns[table_headers[header]] = header
+
+    # Initialize table for timeslot data
+    df = pd.DataFrame(data=data, columns=columns)
+
+    # Make path and create parent directories if they do not exist
+    path = Path.cwd() / f'{Targets.TIMESLOT_DATA}' / f'{TimetableSettings.CAMPUS}' / f'{TimetableSettings.ACADEMIC_YEAR}' / f'{TimetableSettings.CAMPUS}_{TimetableSettings.ACADEMIC_YEAR}_{building_code.name}.csv'
+    path.parent.mkdir(parents=True, exist_ok=True)
+
+    # Write timeslot data to file for building in target directory
+    df.to_csv(path, index=False, mode="w")
+
+
+
 def compute_timeslots(building_code : BuildingCode) -> None:
     # Reads booking data from file for a building, gets the available timeslots for the entire academic year, and writes the timeslots to file
 
-    path = Path.cwd() / f'{Targets.RAW_BOOKING_DATA}' / f'{TimetableSettings.CAMPUS}' / f'{TimetableSettings.ACADEMIC_YEAR}' / f'{TimetableSettings.CAMPUS}_{TimetableSettings.ACADEMIC_YEAR}_{building_code.name}.csv'
-    return 0 # stub
+    # Get path to booking data file
+    read_path = Path.cwd() / f'{Targets.RAW_BOOKING_DATA}' / f'{TimetableSettings.CAMPUS}' / f'{TimetableSettings.ACADEMIC_YEAR}' / f'{TimetableSettings.CAMPUS}_{TimetableSettings.ACADEMIC_YEAR}_{building_code.name}.csv'
+
+    # Read file into dataframe and convert datestrings into datetimes
+    df = pd.read_csv(read_path, parse_dates=["Date"], date_format="%Y-%m-%d")
+
+    # Get all available timeslots for the building
+    timeslots = compute_timeslots_by_rooms(df, building_code)
+
+    # Write timeslots to file
+    write_to_file(timeslots, building_code)
 
 
 
