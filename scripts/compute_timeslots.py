1c40c840922cbc362b5702b789e78498de9c42b1
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-03-20 17:01:31 -0700

    docs: added stub and comments to main in compute_timeslots

--- scripts/compute_timeslots.py
+++ scripts/compute_timeslots.py
@@ -12,3 +12,23 @@ from models import TimetableSettings
 
 
 TARGET_DIR = '/timeslot_data' # Directory to write files to
+
+
+
+def main() -> None:
+    # Reads booking data from file for the desired buildings, and for each building:
+    # - Get all of the rooms in the building
+    # - For each room:
+    #   - Get all of the bookings for the room on a certain date, from the start date to the end date given by the timetable
+    #   - Create timeslots where:
+    #       - The timeslots are within the range given by the timetable [07:00, 22:00]
+    #       - The start and end of each timeslot may be at a half-hour mark (XX:30), or an hour mark (XX:00)
+    #       - Each timeslot spans the largest continuous interval possible between the end of one booking and the start of another booking
+    #       - No two timeslots overlap
+    # Writes the timeslot data to file
+    
+    return 0 # stub
+
+
+
+main()
