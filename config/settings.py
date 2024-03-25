c68c16bc2fc21a7ea26f3fe4dc50fd2a25ae013c
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-03-24 20:04:20 -0700

    refactor: changed name of project directory from "app" to config

--- config/settings.py
+++ config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
