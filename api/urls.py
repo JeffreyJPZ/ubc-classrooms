f5606fa3a8d7a4d4f36b6b12de34750e83a9dfe1
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-04-04 16:12:03 -0700

    feat: added api endpoints for building, room type, and timeslots resources

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,9 @@ register_converter(converters.DateConverter, "date")
 
 urlpatterns = [
     # Version 1
-    path('v1/buildings/<campus:campus>/', 'TODO'),
-    path('v1/roomtypes/<campus:campus>/', 'TODO'),
-    path('v1/timeslots/<campus:campus>/<date:date>/<buildings:buildings>/<roomtypes:roomtypes>/<time:start>/<time:end>/', 'TODO'),
+    path('v1/buildings/<str:campus>/', 'TODO'),
+    path('v1/roomtypes/<str:campus>/', 'TODO'),
+    path('v1/timeslots/<str:campus>/<date:date>/', 'TODO'),
+    
+    # Use query params for start time, end time, building names, and room types
 ]
