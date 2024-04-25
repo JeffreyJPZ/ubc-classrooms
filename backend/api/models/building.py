a8b777612d707c2ef6b8540ecc415117b042a4ef
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-04-25 12:59:13 -0700

    made table model fields

--- backend/api/models/building.py
+++ backend/api/models/building.py
@@ -2,3 +2,9 @@
 Model for the building table
 """
 from django.db import models
+
+class Building(models.Model):
+    campus = models.CharField(max_length=20)
+    campus_code = models.CharField(max_length=20)
+    building_name = models.CharField(max_length=100)
+    building_code = models.CharField(max_length=4)
--- backend/api/models/timeslot.py
+++ backend/api/models/timeslot.py
@@ -2,3 +2,14 @@
 Model for the timeslot table
 """
 from django.db import models
+
+class Timeslot(models.Model):
+    campus = models.CharField(max_length=20)
+    campus_code = models.CharField(max_length=20)
+    building_name = models.CharField(max_length=100)
+    building_code = models.CharField(max_length=4)
+    room_type = models.CharField(max_length=20)
+    room_type_code = models.CharField(max_length=20)
+    date = models.DateField()
+    start = models.TimeField()
+    end = models.TimeField()
