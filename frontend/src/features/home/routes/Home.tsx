e13d113e888d838b39568756b6756ebcc1f32b37
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-05-13 16:26:28 -0700

    chore: removed redundant comment

--- frontend/src/features/home/routes/Home.tsx
+++ frontend/src/features/home/routes/Home.tsx
@@ -12,7 +12,6 @@ export function Home() {
                 <div className="introduction">
                     <h1>Looking for a place to study, take meetings, or meet with friends? UBC Classrooms shows you when and where empty classrooms are available.</h1>
                 </div>
-                {/* Wrap form and timeslot table with a context, with form data as state, use on blur for date*/}
                 <Form/>
             </div>
             <Footer/>
