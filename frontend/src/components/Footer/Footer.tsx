5d5d727889af7defe8bd40aadc06abe351198d68
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-06-21 02:29:01 -0700

    feat: updated disclaimer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.css'
 export function Footer() {
     return (
         <footer className="footer">
-            <p className="disclaimer">Data is obtained from UBC Online Timetable on a weekly basis and may or may not be up-to-date. UBC Classrooms is not affiliated in any way with the University of British Columbia.</p>
+            <p className="disclaimer">Data is obtained from UBC Online Timetable on a weekly basis and may or may not be up-to-date or accurate. UBC Classrooms is not affiliated in any way with the University of British Columbia.</p>
             <div className="authors">Made by Jeffrey Z and Eric Y</div>
         </footer>
     );
