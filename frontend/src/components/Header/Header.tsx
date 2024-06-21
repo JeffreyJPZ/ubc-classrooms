091658bcc4eff311da5259f493347e7249db3e6e
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-06-21 03:08:07 -0700

    feat: added navigation for logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css';
 export function Header() {
     return (
         <header className="header">
-            <a className="logo">UBC Classrooms</a>
+            <a className="logo" href=".">UBC Classrooms</a>
             <nav className="credits">
                 <a className="links" href="https://github.com/JeffreyJPZ/ubc-classrooms" target="_blank">
                     <img src={githubMark} alt="Github repository link"></img>
