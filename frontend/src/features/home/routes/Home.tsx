413ecdc233e15e5af1bc56e2d4e5846d4ba25ccb
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-06-21 18:55:28 -0700

    feat: improved design for tablets, laptops, desktops

--- frontend/src/features/home/routes/Home.tsx
+++ frontend/src/features/home/routes/Home.tsx
@@ -8,10 +8,10 @@ export function Home() {
     return (
         <div className="app">
             <Header/>
+            <div className="introduction">
+                <h1>Looking for a place to study, take meetings, or meet with friends? UBC Classrooms shows you when and where empty classrooms are available.</h1>
+            </div>
             <main className="content">
-                <div className="introduction">
-                    <h1>Looking for a place to study, take meetings, or meet with friends? UBC Classrooms shows you when and where empty classrooms are available.</h1>
-                </div>
                 <Form/>
             </main>
             <Footer/>
