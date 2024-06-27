92f5c019cc646f9a65df7da48d843cb596b13768
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-06-26 20:22:45 -0700

    chore: added dev and production files

---
+++ backend/entrypoint.dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$DB" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# Flush and apply database migrations every time
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
