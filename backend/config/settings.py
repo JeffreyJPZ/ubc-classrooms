6db6c175a98f73d8e1a3e26dc4ee14d8fa2a7261
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-06-26 22:37:10 -0700

    style: remove redundant comment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,8 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Allow CORS between our frontend and our API
-# Change the domain for production
+# Allow CORS between our frontend and our API in development
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000'
 ]
