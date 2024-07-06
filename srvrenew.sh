e1e2b6e7de2561a5c6a7ecc2d475d84f779461e8
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-07-06 16:43:32 -0700

    chore: fixed docker login security

--- srvrenew.sh
+++ srvrenew.sh
@@ -2,17 +2,29 @@
 
 # Pulls Certbot image from Docker Hub and renews SSL certs
 
+echo "Beginning renew script"
+
 # Pull from main branch
-sudo git pull origin main
+sudo git pull origin main && 
+echo "Successfully pulled from origin" ||
+echo "Error with pulling from origin"
 
 # Login to registry
-sudo docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_TOKEN
+(cat prod.env | grep DOCKERHUB_TOKEN | sed "s/DOCKERHUB_TOKEN=//") | sudo docker login -u ubcclassrooms --password-stdin
 
 # Pull latest Certbot image
-sudo docker compose -f compose.srv.yml pull certbot
+sudo docker compose -f compose.srv.yml pull certbot &&
+echo "Successfully pulled image" ||
+echo "Error with pulling image"
 
 # Remove and renew certs
-sudo docker compose -f compose.srv.yml run --rm certbot renew
+sudo docker compose -f compose.srv.yml run --rm certbot renew &&
+echo "Successfully renewed certs" ||
+echo "Error with renewal"
 
 # Reload nginx
-sudo docker compose -f compose.srv.yml exec nginx nginx -s reload
+sudo docker compose -f compose.srv.yml exec nginx nginx -s reload &&
+echo "nginx reloaded" ||
+echo "Error with reloading nginx"
+
+echo "Finished"
