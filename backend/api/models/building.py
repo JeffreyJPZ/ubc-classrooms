a4678b69308c577b4e9937406d9d77f4c71b1e5b
supercoder-dev <mukunda@superagi.com>
2024-07-08 11:18:33 +0530

    Add latitude and longitude to Building model

--- backend/api/models/building.py
+++ backend/api/models/building.py
@@ -7,3 +7,5 @@ class Building(models.Model):
     campus = models.CharField(max_length=4, help_text="UBC campus code")
     building_code = models.CharField(max_length=4, help_text="3-4 letter code of a UBC building")
     building_name = models.CharField(max_length=100, help_text="Full name of a UBC building")
+    latitude = models.FloatField(help_text="Latitude of the building")
+    longitude = models.FloatField(help_text="Longitude of the building")
