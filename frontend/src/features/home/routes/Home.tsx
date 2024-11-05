d8416d81a79f073adfc93fdf7aefe42fdcf64bfe
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-11-05 00:35:56 -0800

    feat: increase map view size

--- frontend/src/features/home/routes/Home.tsx
+++ frontend/src/features/home/routes/Home.tsx
@@ -9,8 +9,7 @@ export function Home() {
         <div className="app">
             <Header/>
             <div className="introduction">
-                <h1>Looking for a place on campus to study, take meetings, or hang out?</h1>
-                <h1>Click on any building marker or use the search fields to get started.</h1>
+                <h1>Looking for a place on campus to study or hang out? Click on any building marker or use the search fields below to get started.</h1>
             </div>
             <main className="main">
                 <Content/>
